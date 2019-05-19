8c24651fdedb1afb8d71891b77eed23f2ab76509
Alexandr Pogodin <37239276+AlexandrPogodin@users.noreply.github.com>
2019-05-19 21:17:29 +0400

    Update models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task = models.CharField(max_length=30)
+    description = models.CharField(max_length=50)
+
+    create = models.DateField(auto_now=true)
+    exp = models.DateTimeField()
+
+    def __str__(self):
+        return self.task
