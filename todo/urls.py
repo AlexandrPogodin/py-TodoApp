4355d72674bde06304b7713d6a435d99734c6b3e
Alexandr Pogodin <37239276+AlexandrPogodin@users.noreply.github.com>
2019-05-22 11:41:07 +0400

    Update [*]

--- todo/urls.py
+++ todo/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add, name='add'),
+    path('contacts/', views.contacts, name='contacts'),
     path('done/<int:id>/', views.done),
     path('undo/<int:id>/', views.undo),
     path('delete/<int:id>/', views.delete),
