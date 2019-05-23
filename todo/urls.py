940f14a1cfccfc41c756facc0a98428310968ebc
Alexandr Pogodin <37239276+AlexandrPogodin@users.noreply.github.com>
2019-05-23 21:59:24 +0400

    Add signup and login

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('add/', views.add, name='add'),
-    path('contacts/', views.contacts, name='contacts'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', views.signup, name='signup'),
     path('done/<int:id>/', views.done),
     path('undo/<int:id>/', views.undo),
     path('delete/<int:id>/', views.delete),
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.http import HttpResponseNotFound
 from .models import Task
 from .forms import TaskForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     if request.method == "POST":
@@ -36,8 +38,19 @@ def index(request):
 
     return render(request, 'todo/index.html', context)
 
-def contacts(request):
-    return render(request, 'todo/contacts.html')
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 def add(request):
     form = TaskForm()
