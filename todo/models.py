700c331f59384a89f7bc72f1ed8b7bd53f598264
Alexandr Pogodin <37239276+AlexandrPogodin@users.noreply.github.com>
2019-05-24 19:22:43 +0400

    Completed the authorization system

--- todo/models.py
+++ todo/models.py
@@ -5,4 +5,5 @@ class Task(models.Model):
     task = models.CharField(max_length = 50)
     description = models.CharField(max_length = 100, blank=True)
     done = models.BooleanField(default = False)
+    author = models.CharField(max_length = 50, default='')
     date = models.DateTimeField(auto_now = True)
